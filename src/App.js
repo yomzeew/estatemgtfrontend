7e91c03f8da4e970eae639325005faf11958b720
yomzeew <yomzeew@gmail.com>
2023-11-26 19:46:44 +0100

    updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './component/weatherapp';
+
+function App() {
+  return (
+    <WeatherApp/>
+  );
+}
+
+export default App;
