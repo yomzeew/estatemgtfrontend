dde8486566455f0f06849145758699ae224beafa
Babayomi Oluwasuyi <61556760+yomzeew@users.noreply.github.com>
2024-01-19 14:06:39 +0100

    Update api.js

--- src/component/api/api.js
+++ src/component/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const api = axios.create({
- baseURL:process.env.REACT_APP_API_URL,
-//baseURL: 'https://backend.fayah.app',
+ //baseURL:process.env.REACT_APP_API_URL,
+baseURL: 'https://backend.fayah.app',
   headers: {
     'Content-type': 'application/json',
     
